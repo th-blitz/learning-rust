5f8f5a54d716fca0a56655edee0706153360205e
ThBlitz <preethamrakshith11@gmail.com>
2023-02-24 17:47:23 +0530

    RESET: git cache & completed ch2

---
+++ the-rust-book/src/ch2.rs
@@ -0,0 +1,39 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+pub fn run() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Please input your guess.");
+
+    loop {
+
+        let mut guess = String::new();
+
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("not a number");
+                continue;
+            },
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small !"),
+            Ordering::Equal => { 
+                println!("You win !");
+                break; 
+            },
+            Ordering::Greater => println!("Too great !"),
+        }
+    }
+}
---
+++ the-rust-book/src/main.rs
@@ -0,0 +1,7 @@
+mod ch2;
+mod ch3;
+
+fn main() {
+    // ch2::run(); 
+    ch3::run();
+}
