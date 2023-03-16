1316d1c7ead64a79a31f24f78ed25a440d17a0af
ThBlitz <preethamrakshith11@gmail.com>
2023-03-16 14:58:25 +0530

    chapter 3: variable, functions, expressions and loops

--- the-rust-book/src/ch3.rs
+++ the-rust-book/src/ch3.rs
@@ -1,8 +1,22 @@
 
 
 pub fn run() {
-    let mut x = 5;
-    println!("The value of x is : {}", x);
-    x = 6;
-    println!("The value of x is : {}", x);
+    // let mut x = 5;
+    // println!("The value of x is : {}", x);
+    // x = 6;
+    // println!("The value of x is : {}", x);
+     
+    
+    // let y = 5;
+    // let y = y + 1;
+
+    // {
+    //     let y = y * 2;
+    //     println!("the value of y: {y}");
+    // }
+
+    // println!("The value of y is : {y}");
+
+    let x: char = '😻';
+    println!("x : {x}");
 }
