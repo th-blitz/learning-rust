3ac0ac2f3f1f2e38158f448e46d7921af5e5bc11
ThBlitz <preethamrakshith11@gmail.com>
2023-03-24 13:32:45 +0530

    chapter 5: structs and impl

---
+++ the-rust-book/src/ch5.rs
@@ -0,0 +1,71 @@
+
+#[derive(Debug)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+struct AlwaysEqual;
+
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+pub fn run() {
+    
+    let mut user1 = User {
+        active: true,
+        username: String::from("user1"),
+        email: String::from("user1@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("another@example.com");
+
+    let user2 = User {
+        email: String::from("user2@example.com"),
+        ..user1
+    };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
+
+    println!("struct user1 : {:#?}", user2);
+
+
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
+
+    println!("The area of rect1 {}", rect1.area());
+
+}
--- the-rust-book/src/main.rs
+++ the-rust-book/src/main.rs
@@ -1,9 +1,12 @@
 mod ch2;
 mod ch3;
 mod ch4;
+mod ch5;
 
 fn main() {
     // ch2::run(); 
     // ch3::run();
-    ch4::run();
+    // ch4::run();
+    ch5::run();
+
 }
